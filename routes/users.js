39b0dd36b56d5b060fd73c970d87fda7885e859a
Jenny Zonka <jennycade@gmail.com>
2022-02-27 10:42:19 -0800

    Get all users works for authenitcated users with admin role, otherwise returns 403

--- routes/users.js
+++ routes/users.js
@@ -5,18 +5,29 @@ var router = express.Router();
 const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User
-      .find('displayName createdAt')
-      .sort('-createdAt')
-      .exec();
+router.get('/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      // check for admin role
+      if (!req.user.roles.includes('admin')) {
+        const err = new Error('You do not have permission to see all users');
+        err.status = 403;
+        throw err;
+      } else {
+          const users = await User
+            .find()
+            .select('-password')
+            .sort('-createdAt')
+            .exec();
 
-    res.json(users);
-  } catch (err) {
-    return next(err);
+          res.json(users);
+      }
+    } catch (err) {
+      return next(err);
+    }
   }
-});
+);
 
 // create new user, a.k.a. sign up
 router.post('/',
