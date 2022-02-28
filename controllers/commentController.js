371ccf6dbb10db0dff384183d70fc2801f75ec07
Jenny Zonka <jennycade@gmail.com>
2022-02-28 14:04:39 -0800

    Move logic to throw validation error from commentController to validationController

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,6 @@
-const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
+
+const validationController = require('./validationController');
 
 exports.validate = () => {
   return [
@@ -15,20 +17,7 @@ exports.validate = () => {
       .exists({checkFalsy: true}).withMessage('Post required')
       .trim().escape(),
 
-    (req, res, next) => {
-      try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          const errorMessages = errors.array().map(fieldError => fieldError.msg).join(', ');
+    validationController.throwValidationErrors,
     
-          const err = new Error(`Validation errors: ${errorMessages}`);
-          err.status = 400;
-          throw err;
-        }
-        return next();
-      } catch (err) {
-        return next(err);
-      }
-    },
   ];
 };
